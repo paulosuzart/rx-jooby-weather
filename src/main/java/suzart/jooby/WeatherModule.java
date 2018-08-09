3a2aaf9593de68a1c271cb3b7ea80b42cb0ed22d
Paulo Suzart <paulosuzart@gmail.com>
2018-08-09 22:21:38 +0200

    Org imports

--- src/main/java/suzart/jooby/WeatherModule.java
+++ src/main/java/suzart/jooby/WeatherModule.java
@@ -1,18 +1,15 @@
 package suzart.jooby;
 
 import com.google.inject.Binder;
-import com.google.inject.Provider;
 import com.typesafe.config.Config;
-
 import org.asynchttpclient.AsyncHttpClient;
 import org.jooby.Env;
 import org.jooby.Jooby;
-
-import static org.asynchttpclient.Dsl.*;
-
 import suzart.jooby.clients.openwheather.WeatherClient;
 import suzart.jooby.controllers.WeatherAvgController;
 import suzart.jooby.services.WeatherAvgService;
+
+import static org.asynchttpclient.Dsl.asyncHttpClient;
 
 public class WeatherModule implements Jooby.Module {
 
